4687b63025755f5a7eb4b4426f911c59e5091b8e
EDUARDO MARTINEZ COLLAZO <143230456+Edi712g@users.noreply.github.com>
2025-03-09 11:07:36 -0600

    comentarios

--- practica 1.py
+++ practica 1.py
@@ -1,18 +1,22 @@
-import cv2
+#librerias
+import cv2 
 import numpy as np
 import matplotlib.pyplot as plt 
 
-img = cv2.imread('fotos/bill.jpg',cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('fotos/bill.jpg',cv2.IMREAD_GRAYSCALE) #asignar a la variable img los datos de bill que se encuenta en la carpeta fotos, donde se va a hacer la lectura a base de grises
 
+#otros tipos de interpretacion de las imagenes
 #IMREAD_COLOR = 1
 #IMREAD_UNCHANGED = -1
 
-cv2.imshow('holam mundo',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#nombre de la ventana y variable que se va a mostrar con la funcion show de cv2
+#cv2.imshow('holam mundo',img)
+#cv2.waitKey(0)  #espera de alguna tecla 
+#cv2.destroyAllWindows() #cerrar el ejecutable
 
-#plt.imshow(img, cmap='gray',interpolation='bicubic')
-#plt.plot([50,100],[80,100],'c',linewidth=5)
-#plt.show()
+#imprecion de img con interpolacion 
+plt.imshow(img, cmap='plasma',interpolation='bicubic') #mostrar la imagen con una referencia de color(como si fuera un filto)
+plt.plot([275,50],[280,50],'c',linewidth=5)    #incertar en la imagen una linea en las cordenadas y con line de grosor 5
+plt.show()
 
-cv2.imwrite('fotos/bill mejorado.png',img)
+#cv2.imwrite('fotos/bill mejorado.png',img)
