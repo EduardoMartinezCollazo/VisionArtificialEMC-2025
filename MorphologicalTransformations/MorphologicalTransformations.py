4b7f09539cfdc15f86fdfa83b2a6428f77c4460b
EDUARDO MARTINEZ COLLAZO <143230456+Edi712g@users.noreply.github.com>
2025-06-11 14:09:44 -0600

    00-actualizacion

--- MorphologicalTransformations/MorphologicalTransformations.py
+++ MorphologicalTransformations/MorphologicalTransformations.py
@@ -8,8 +8,8 @@ while(1):
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
-    lower_red = np.array([30,150,50])
-    upper_red = np.array([255,255,180])
+    lower_red = np.array([0,150,150])
+    upper_red = np.array([20,250,250])
     
     mask = cv2.inRange(hsv, lower_red, upper_red)
     res = cv2.bitwise_and(frame,frame, mask= mask)
@@ -33,7 +33,7 @@ cap.release()
 cap = cv2.VideoCapture(0)
 
 while(1):
-
+## muestra simultanea de erosio y dilatacion en apertura (pequeños) y dilatacion con erocion en cierre()
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
